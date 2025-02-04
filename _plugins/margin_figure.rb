432cda92e6214a5849bc0e6a115576a6b37ed463
tillmanj <jontillman23@gmail.com>
2025-02-04 14:45:48 +0100

    first pass responsive typography
    Signed-off-by: tillmanj <jontillman23@gmail.com>

--- _plugins/margin_figure.rb
+++ _plugins/margin_figure.rb
@@ -13,16 +13,23 @@ module Jekyll
       end
   
       def render(context)
+        # Gather settings
+        site = context.registers[:site]
+        converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+        
         baseurl = context.registers[:site].config['baseurl']
         pageTitle = context.registers[:page]['title']
+        label = Kramdown::Document.new(@text[2],{remove_span_html_tags:false}).to_html # render markdown in caption
+        label = converter.convert(label).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
+        
         if @text[1].start_with?('http://', 'https://', '//')
           "<label for='#{@text[0]}' class='margin-toggle'>&#8853;</label>"+
           "<input type='checkbox' id='#{@text[0]}' class='margin-toggle'/>"+
-          "<span class='marginnote'><a href='#{@text[1]}' data-lightbox='#{pageTitle}' data-alt='#{@text[2]}' data-title='#{@text[2]}' title='#{@text[2]}'><img class='fullwidth' src='#{@text[1]}'/></a><br>#{@text[2]}</span>"
+          "<span class='marginnote'><a href='#{@text[1]}' data-lightbox='#{pageTitle}' data-alt='#{label}' data-title='#{label}' title='#{label}'><img class='fullwidth' src='#{label}'/></a><br>#{@text[2]}</span>"
         else
           "<label for='#{@text[0]}' class='margin-toggle'>&#8853;</label>"+
           "<input type='checkbox' id='#{@text[0]}' class='margin-toggle'/>"+
-          "<span class='marginnote'><a href='#{baseurl}/#{@text[1]}' data-lightbox='#{pageTitle}' data-alt='#{@text[2]}' data-title='#{@text[2]}' title='#{@text[2]}'><img class='fullwidth' src='#{baseurl}/#{@text[1]}'/></a><br>#{@text[2]}</span>"
+          "<span class='marginnote'><a href='#{baseurl}/#{@text[1]}' data-lightbox='#{pageTitle}' data-alt='#{label}' data-title='#{label}' title='#{label}'><img class='fullwidth' src='#{baseurl}/#{@text[1]}'/></a><br>#{label}</span>"
         end
       end
     end
