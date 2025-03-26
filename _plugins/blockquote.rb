5fad01db9f700bc50155dda349f68033922b947a
tillmanj <jontillman23@gmail.com>
2025-03-26 14:21:37 +0100

    better handle quotes without authors but with sources
    Signed-off-by: tillmanj <jontillman23@gmail.com>

--- _plugins/blockquote.rb
+++ _plugins/blockquote.rb
@@ -3,36 +3,38 @@
 ## Usage {% blockquote 'unique-id-for-blockquote' 'text-body-of-quote' 'author-of-quote' 'citation-of-quote' 'marginalia' %}
 #
 module Jekyll
-    class RenderBlockquoteTag < Liquid::Tag
-      require "shellwords"
-      require "stringio"
-      require "kramdown"
-  
-      def initialize(tag_name, text, tokens)
-        super
-        @text = text.shellsplit
+  class RenderBlockquoteTag < Liquid::Tag
+    require "shellwords"
+    require "stringio"
+    require "kramdown"
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text.shellsplit
+    end
+    
+    def render(context)
+      # Gather settings
+      site = context.registers[:site]
+      converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+
+      baseurl = context.registers[:site].config['baseurl']
+      quote = Kramdown::Document.new(@text[1],{remove_span_html_tags:false}).to_html # render markdown in caption
+      quote = converter.convert(quote).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
+      author = Kramdown::Document.new(@text[2],{remove_span_html_tags:true}).to_html # render markdown in source
+      author = converter.convert(author).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
+      if author.length > 0
+        author = author + ", "
       end
-      
-      def render(context)
-        # Gather settings
-        site = context.registers[:site]
-        converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
-  
-        baseurl = context.registers[:site].config['baseurl']
-        quote = Kramdown::Document.new(@text[1],{remove_span_html_tags:true}).to_html # render markdown in caption
-        quote = converter.convert(quote).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
-        author = Kramdown::Document.new(@text[2],{remove_span_html_tags:true}).to_html # render markdown in source
-        author = converter.convert(author).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
-        source = Kramdown::Document.new(@text[3],{remove_span_html_tags:true}).to_html # render markdown in source
-        source = converter.convert(source).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
-        marginalia = Kramdown::Document.new(@text[4],{remove_span_html_tags:true}).to_html # render markdown in source
-        marginalia = converter.convert(marginalia).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
-          "<div class='blockquote'><blockquote id='#{@text[0]}'><span class='marginnote'>#{marginalia} </span><p>#{quote}</p>"+
-          ""+
-          "<footer>#{author}, "+"<cite>#{source}</cite></footer></blockquote></div>"
-      end
+      source = Kramdown::Document.new(@text[3],{remove_span_html_tags:true}).to_html # render markdown in source
+      source = converter.convert(source).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
+      marginalia = Kramdown::Document.new(@text[4],{remove_span_html_tags:true}).to_html # render markdown in source
+      marginalia = converter.convert(marginalia).gsub(/<\/?p[^>]*>/, "").chomp # remove <p> tags from render output
+        "<div class='blockquote'><blockquote id='#{@text[0]}'><span class='marginnote'>#{marginalia} </span><p>#{quote}</p>"+
+        ""+
+        "<footer>#{author}"+"<cite>#{source}</cite></footer></blockquote></div>"
     end
   end
-  
-  Liquid::Template.register_tag('blockquote', Jekyll::RenderBlockquoteTag)
-  
+end
+
+Liquid::Template.register_tag('blockquote', Jekyll::RenderBlockquoteTag)
