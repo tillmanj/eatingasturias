6adc572489fdbe23390e9e467d2bd84b0e2c8aab
tillmanj <jontillman23@gmail.com>
2025-06-06 11:19:58 +0200

    margin figure automation
    Signed-off-by: tillmanj <jontillman23@gmail.com>

--- _plugins/margin_figure.rb
+++ _plugins/margin_figure.rb
@@ -29,7 +29,7 @@ module Jekyll
         else
           "<label for='#{@text[0]}' class='margin-toggle'>&#8853;</label>"+
           "<input type='checkbox' id='#{@text[0]}' class='margin-toggle'/>"+
-          "<span class='marginnote'><a href='#{baseurl}#{@text[1]}' data-lightbox='#{pageTitle}' data-alt='#{label}' data-title='#{label}' title='#{label}'><img class='fullwidth' src='#{baseurl}/#{@text[1]}'/></a><br>#{label}</span>"
+          "<span class='marginnote'><a href='#{baseurl}#{@text[1]}' data-lightbox='#{pageTitle}' data-alt='#{label}' data-title='#{label}' title='#{label}'><img class='fullwidth' src='#{baseurl}#{@text[1]}'/></a><br>#{label}</span>"
         end
       end
     end
